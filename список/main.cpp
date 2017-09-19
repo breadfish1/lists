ef9b42a0d30f99f86936c5319202d5dcd64391b3
Алексей Белянин <alekseybelyanin@MacBook-Pro-Aleksej.local>
2017-09-19 20:53:19 +0300

    Initial Commit

---
+++ список/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  список
+//
+//  Created by Алексей Белянин on 19/09/2017.
+//  Copyright © 2017 Алексей Белянин. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
